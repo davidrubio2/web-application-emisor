cfa0a04eae320ccda65d766aaba2efcc3d798186
david <davidrubio2@gmail.com>
2018-07-01 12:10:42 -0600

    add try catch in the delete,add, and alter functions

--- Models/DataAccessEmisor.cs
+++ Models/DataAccessEmisor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace webappemisor
 {
@@ -6,22 +7,57 @@ namespace webappemisor
     {
         string sConnection = "server=localhost;user id=root;password=;persistsecurityinfo=True;port=3306;database=test_candidate;SslMode=none";
 
-        public void GetAllEmisor()
+        public IEnumerable<Emisor> GetAllEmisor()
         {
+            List<Emisor> lstEmisor = new List<Emisor>();
+          
+
+            return lstEmisor;
+        }
+        public string AddEmisor()
+        {
+            string sRespuesta = "Ok";
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                sRespuesta = ex.ToString();
+            }
+
+            return sRespuesta;
+        }
+
+        public string DeleteEmisor()
+        {
+            string sRespuesta = "Ok";
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                sRespuesta = ex.ToString();
+            }
+
+            return sRespuesta;
 
         }
-        public void AddEmisor()
+
+        public string AlterEmisor()
         {
+            string sRespuesta = "Ok";
+            try
+            {
 
-        }
+            }
+            catch (Exception ex)
+            {
+                sRespuesta = ex.ToString();
+            }
 
-        public void DeleteEmisor()
-        {
-
-        }
-
-        public void AlterEmisor()
-        {
+            return sRespuesta;
 
         }
     }
