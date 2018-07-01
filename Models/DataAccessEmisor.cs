08bab532ec7240f2a819338ec6f500163f470808
david <davidrubio2@gmail.com>
2018-07-01 12:02:41 -0600

    i add the connection string and i add four function getall,add,delte and alter emisor

--- Models/DataAccessEmisor.cs
+++ Models/DataAccessEmisor.cs
@@ -4,6 +4,25 @@ namespace webappemisor
 {
     public class DataAccessEmisor
     {
-        
+        string sConnection = "server=localhost;user id=root;password=;persistsecurityinfo=True;port=3306;database=test_candidate;SslMode=none";
+
+        public void GetAllEmisor()
+        {
+
+        }
+        public void AddEmisor()
+        {
+
+        }
+
+        public void DeleteEmisor()
+        {
+
+        }
+
+        public void AlterEmisor()
+        {
+
+        }
     }
 }
